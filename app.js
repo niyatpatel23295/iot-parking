5e421fb813e72b37b14b35072a8a41bb4dfd90c3
niyatpatel23295 <niyatpatel23295@gmail.com>
2018-12-08 21:11:21 -0800

    Bub fix

--- app.js
+++ app.js
@@ -5,20 +5,48 @@ var db = require('./db.js');
 
 app.get('/getstatus', function(req, res){
 	let serial = req.query.serial;
-	db.pool.query('SELECT * from devices WHERE serial like "' +serial + '"' , function (error, results, fields) {
-	  if (error) throw error;
-	  res.send(results[0])
-	});
+	try {
+		db.pool.query('SELECT * from devices WHERE serial like "' +serial + '"' , function (error, results, fields) {
+		  if (error) {
+		  	console.log(error);
+		  	res.send(400, {message: "Error occured"})
+		  }
+		  else{
+		  	res.send(results[0])
+		  }
+		  
+		});
+		// statements
+	} catch(e) {
+		res.send(400, {message: "Error occured"})
+		// statements
+		console.log(e);
+	}
+
 });
 
 app.get('/setstatus', function(req, res){
 	let serial = req.query.serial;
 	let status = req.query.status;
 
-	db.pool.query('UPDATE devices SET status = ' +status + ' WHERE serial like "' +serial + '"' , function (error, results, fields) {
-	  if (error) throw error;
-	  res.send(results)
-	});
+	try {
+		db.pool.query('UPDATE devices SET status = ' +status + ' WHERE serial like "' +serial + '"' , function (error, results, fields) {
+		  if (error) {
+		  	console.log(error);
+		  	res.send(400, {message: "Error occured"})
+		  }
+		  else{
+		  	res.send(results)
+		  }
+		  
+		});
+		// statements
+	} catch(e) {
+		// statements
+		res.send(400, {message: "Error occured"})
+		console.log(e);
+	}
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
